cfb70ba6b3f3ee939687c397fbe6ee47471e9af6
Vinicius <Vinicius@DESKTOP-BSVG166>
2024-05-05 22:05:15 -0300

    Realizando teste

--- src/main/java/br/com/fiap/brindes/entity/Categoria.java
+++ src/main/java/br/com/fiap/brindes/entity/Categoria.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+// 
 @Entity
 @Table(name = "tb_categorias")
 public class Categoria {
